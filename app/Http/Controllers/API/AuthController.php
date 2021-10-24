38cdbb2a596ddc77a09cf659aa40777b41c83cf5
Gianluca <gl.tiengo@gmail.com>
2021-10-24 15:01:47 +0200

    adding admin user

--- app/Http/Controllers/API/AuthController.php
+++ app/Http/Controllers/API/AuthController.php
@@ -6,40 +6,98 @@ namespace App\Http\Controllers\API;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Models\User;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Validator;
 
 class AuthController extends Controller
 {
-    public function register(Request $request)
+//    public function register(Request $request)
+//    {
+//        $validatedData = $request->validate([
+//            'name' => 'required|max:55',
+//            'email' => 'email|required|unique:users',
+//            'password' => 'required'
+//        ]);
+//
+//        $validatedData['password'] = Hash::make($request->password);
+//
+//        $user = User::create($validatedData);
+//
+//        $accessToken = $user->createToken('authToken')->accessToken;
+//
+//        return response(['user' => $user, 'access_token' => $accessToken], 201);
+//    }
+//
+//    public function login(Request $request)
+//    {
+//        $loginData = $request->validate([
+//            'email' => 'email|required',
+//            'password' => 'required'
+//        ]);
+//
+//        if (!auth()->attempt($loginData)) {
+//            return response(['message' => 'This User does not exist, check your details'], 400);
+//        }
+//
+//        $accessToken = auth()->user()->createToken('authToken')->accessToken;
+//
+//        return response(['user' => auth()->user(), 'access_token' => $accessToken]);
+//    }
+
+    public function index()
     {
-        $validatedData = $request->validate([
-            'name' => 'required|max:55',
-            'email' => 'email|required|unique:users',
-            'password' => 'required'
-        ]);
-
-        $validatedData['password'] = Hash::make($request->password);
-
-        $user = User::create($validatedData);
-
-        $accessToken = $user->createToken('authToken')->accessToken;
-
-        return response(['user' => $user, 'access_token' => $accessToken], 201);
+        return response()->json(User::with(['orders'])->get());
     }
 
     public function login(Request $request)
     {
-        $loginData = $request->validate([
-            'email' => 'email|required',
-            'password' => 'required'
-        ]);
+        $status = 401;
+        $response = ['error' => 'Wrong password or user name please try again.'];
 
-        if (!auth()->attempt($loginData)) {
-            return response(['message' => 'This User does not exist, check your details'], 400);
+        if (Auth::attempt($request->only(['email', 'password']))) {
+            $status = 200;
+            $response = [
+                'user' => Auth::user(),
+                'token' => Auth::user()->createToken('bigStore')->accessToken,
+            ];
         }
 
-        $accessToken = auth()->user()->createToken('authToken')->accessToken;
-
-        return response(['user' => auth()->user(), 'access_token' => $accessToken]);
+        return response()->json($response, $status);
     }
+
+    public function register(Request $request)
+    {
+        $validator = Validator::make($request->all(), [
+            'name' => 'required|max:50',
+            'email' => 'required|email|unique:users',
+            'password' => 'required|min:6'
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json( $validator->errors(), 401);
+        }
+
+        $data = $request->only(['name', 'email', 'password']);
+        $data['password'] = bcrypt($data['password']);
+
+        $user = User::create($data);
+        $user->is_admin = 0;
+
+        $token = $user->createToken('bigStore')->accessToken;
+
+        return response()->json(['token'=>$token],200);
+
+    }
+
+    public function show(User $user)
+    {
+        return response()->json($user);
+    }
+
+    public function showOrders(User $user)
+    {
+        return response()->json($user->orders()->with(['product'])->get());
+    }
+
 }
--- app/Models/User.php
+++ app/Models/User.php
@@ -2,15 +2,18 @@
 
 namespace App\Models;
 
-use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Laravel\Cashier\Billable;
 use Laravel\Passport\HasApiTokens;
+use App\Models\Order;
 
 class User extends Authenticatable
 {
-    use HasApiTokens, HasFactory, Notifiable;
+    use HasApiTokens, HasFactory, Notifiable, Billable, SoftDeletes;
 
     /**
      * The attributes that are mass assignable.
@@ -41,4 +44,9 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    public function orders()
+    {
+        return $this->hasMany(Order::class);
+    }
 }
